9da27b8bf6ca046fa27cdec8467e1f63666b499f
divyamishra22 <115539014+divyamishra22@users.noreply.github.com>
2023-02-20 20:32:28 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package gamepackage;
+
+public class Main {
+	public static void main(String[] args) {
+		Player1 player = new Player1("Abhay","sword",100);
+		//System.out.println(player.getName());
+		//System.out.println(player.getHealth());
+		//System.out.println(player.getWeapon());
+		
+		
+		player.damageByGun1();
+		player.damageByGun1();
+		player.damageByGun2();
+		player.getHealth();
+	}
+
+}
